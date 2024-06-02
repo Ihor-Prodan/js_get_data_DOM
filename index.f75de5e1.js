58426c43a8c88833b30e9cd6ebb4439675eb7265
Ihor Prodan <prodanigor0@gmail.com>
2024-06-02 21:34:37 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const population = document.querySelectorAll(".population");
+let totalPopulation = 0;
+population.forEach((element)=>{
+    const populationStr = element.textContent.replace(/,/g, "");
+    const populationNum = parseInt(populationStr, 10);
+    totalPopulation += populationNum;
+});
+const averagePopulation = totalPopulation / population.length;
+document.querySelector(".total-population").textContent = totalPopulation.toLocaleString();
+document.querySelector(".average-population").textContent = averagePopulation.toLocaleString();
+
+//# sourceMappingURL=index.f75de5e1.js.map
